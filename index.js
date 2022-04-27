80afa9019cd116c485225af43a38a72ec5b3a40e
acfernanda <acfernanda98@gmail.com>
2022-04-27 20:58:46 -0300

    get participants

--- index.js
+++ index.js
@@ -1,16 +1,35 @@
-import express from "express";
+import express, { json } from "express";
 import cors from "cors";
 import chalk from "chalk";
+import dotenv from "dotenv";
+dotenv.config();
+
+import { MongoClient } from "mongodb";
 
 const app = express();
 app.use(cors());
+app.use(json());
+
+let db = null;
+const mongoClient = new MongoClient(process.env.MONGO_URI);
 
 app.post("/participants", (req, res) => {
   res.send("post participants");
 });
 
-app.get("/participants", (req, res) => {
-  res.send("get participants");
+app.get("/participants", async (req, res) => {
+  try {
+    await mongoClient.connect();
+    const db = mongoClient.db("batepapo_uol");
+    const participantsCollection = db.collection("participants");
+    const participants = await participantsCollection.find({}).toArray();
+
+    res.send(participants);
+    mongoClient.close();
+  } catch (e) {
+    res.status(500).send("Não foi possível encontrat os participantes.");
+    mongoClient.close();
+  }
 });
 
 app.post("/messages", (req, res) => {
