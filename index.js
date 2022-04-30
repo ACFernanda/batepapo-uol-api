3e242ec53bb9170dc309704031575020580080d3
acfernanda <acfernanda98@gmail.com>
2022-04-30 18:09:35 -0300

    get messages corrected with limit

--- index.js
+++ index.js
@@ -77,11 +77,52 @@ app.get("/participants", async (req, res) => {
   }
 });
 
-app.post("/messages", (req, res) => {
-  res.send("post messages");
+app.post("/messages", async (req, res) => {
+  const { to, text, type } = req.body;
+  const from = req.headers.user;
+
+  try {
+    await mongoClient.connect();
+    const db = mongoClient.db("batepapo_uol");
+
+    // const checkUser = await db
+    //   .collection("participants")
+    //   .findOne(from)
+    //   .toArray();
+
+    // const messageSchema = joi.object({
+    //   to: joi.string().required(),
+    //   text: joi.string().required(),
+    //   type: joi.string().required().valid("message", "private_message"),
+    //   from: joi.string().required().valid(checkUser[0]),
+    // });
+
+    // const validation = messageSchema.validate(req.body, {
+    //   abortEarly: true,
+    // });
+
+    // if (validation.error) {
+    //   res.sendStatus(422);
+    //   return;
+    // }
+
+    await db.collection("messages").insertOne({
+      from: from,
+      to: to,
+      text: text,
+      type: type,
+      time: dayjs().format("HH:mm:ss"),
+    });
+
+    res.sendStatus(201);
+    mongoClient.close();
+  } catch (e) {
+    res.status(500).send(e);
+    mongoClient.close();
+  }
 });
 
-app.get("/messages/", async (req, res) => {
+app.get("/messages", async (req, res) => {
   const { user } = req.headers;
   const limit = req.query.limit;
 
@@ -89,24 +130,21 @@ app.get("/messages/", async (req, res) => {
     await mongoClient.connect();
     const db = mongoClient.db("batepapo_uol");
     const messagesCollection = db.collection("messages");
-    const messages = await messagesCollection.find().toArray();
-    const messagesFiltered = messages
-      .reverse()
-      .slice(0, () => {
-        if (limit !== undefined) {
-          return parseInt(req.query.limit);
-        } else {
-          return messages.length;
-        }
-      })
-      .filter((message) => {
-        return (
-          message.to === user || message.to === "Todos" || message.from === user
-        );
-      });
+    const messages = await messagesCollection.find({}).toArray();
+    const messagesFiltered = messages.filter((message) => {
+      return (
+        message.to === user || message.to === "Todos" || message.from === user
+      );
+    });
 
-    res.send(messagesFiltered.reverse());
-    mongoClient.close();
+    if (limit !== undefined) {
+      const limitedMessages = messagesFiltered.slice(-limit);
+      res.send(limitedMessages);
+      mongoClient.close();
+    } else {
+      res.send(messagesFiltered);
+      mongoClient.close();
+    }
   } catch (e) {
     res.status(500).send("Não foi possível encontrar as mensagens.");
     mongoClient.close();
