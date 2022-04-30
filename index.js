d125d8958d69bc3ded8faf28235e83d0e55994f6
acfernanda <acfernanda98@gmail.com>
2022-04-30 13:26:56 -0300

    validation participant's name

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import chalk from "chalk";
 import dotenv from "dotenv";
 import dayjs from "dayjs";
+import joi from "joi";
 dotenv.config();
 
 import { MongoClient } from "mongodb";
@@ -11,14 +12,34 @@ const app = express();
 app.use(cors());
 app.use(json());
 
-let db = null;
-const mongoClient = new MongoClient(process.env.MONGO_URI);
+const mongoClient = new MongoClient(process.env.MONGO_URI); // COLOCAR ISSO DENTRO DE CADA REQUISIÇÃO
 
 app.post("/participants", async (req, res) => {
   const { name } = req.body;
+
+  const participantSchema = joi.object({
+    name: joi.string().required(),
+  });
+
+  const validation = participantSchema.validate(req.body);
+
+  if (validation.error) {
+    res.sendStatus(422);
+    return;
+  }
+
   try {
     await mongoClient.connect();
     const db = mongoClient.db("batepapo_uol");
+
+    const participantExists = await db
+      .collection("participants")
+      .findOne({ name: name });
+
+    if (participantExists) {
+      res.status(409).send("Nome de usuário já está sendo utilizado.");
+      return;
+    }
 
     await db.collection("participants").insertOne({
       name: name,
@@ -75,8 +96,32 @@ app.get("/messages", async (req, res) => {
   }
 });
 
-app.post("/status", (req, res) => {
-  res.send("post status");
+app.post("/status", async (req, res) => {
+  const { user } = req.headers;
+  try {
+    await mongoClient.connect();
+    const db = mongoClient.db("batepapo_uol");
+    const participantsCollection = db.collection("participants");
+    const participant = await participantsCollection.findOne({
+      name: user,
+    });
+
+    if (!participant) {
+      res.sendStatus(404);
+      mongoClient.close();
+      return;
+    }
+
+    await participantsCollection.updateOne(participant, {
+      $set: { lastStatus: Date.now() },
+    });
+
+    res.sendStatus(200);
+    mongoClient.close();
+  } catch (error) {
+    res.status(500).send(error);
+    mongoClient.close();
+  }
 });
 
 app.listen(5000, () => {
