cead9fc6264792ce17264cc15bb8395af24924d9
acfernanda <acfernanda98@gmail.com>
2022-04-30 21:31:15 -0300

    post /messages correction

--- index.js
+++ index.js
@@ -81,6 +81,8 @@ app.post("/messages", async (req, res) => {
   const { to, text, type } = req.body;
   const from = req.headers.user;
 
+  const message = { ...req.body, from };
+
   try {
     await mongoClient.connect();
     const db = mongoClient.db("batepapo_uol");
@@ -90,18 +92,23 @@ app.post("/messages", async (req, res) => {
       .find({ name: from })
       .toArray();
 
+    if (!checkUser.length) {
+      res.sendStatus(422);
+      return;
+    }
+
     const messageSchema = joi.object({
       to: joi.string().required(),
       text: joi.string().required(),
-      type: joi.string().required().valid("message", "private_message"),
-      from: joi.required().valid(checkUser[0].name),
+      type: joi.required().valid("message", "private_message"),
+      from: joi.string().required(),
     });
 
-    const validation = messageSchema.validate(req.body, {
-      abortEarly: true,
+    const validate = messageSchema.validate(message, {
+      abortEarly: false,
     });
 
-    if (validation.error) {
+    if (validate.error) {
       res.sendStatus(422);
       return;
     }
@@ -117,6 +124,7 @@ app.post("/messages", async (req, res) => {
     res.sendStatus(201);
     mongoClient.close();
   } catch (e) {
+    console.log(e);
     res.status(500).send(e);
     mongoClient.close();
   }
